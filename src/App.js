e4beb5de10355485b8f4a42456fcb6482c0e9a14
h-faber <hfaberlu@gmail.com>
2024-09-20 21:05:20 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 function App() {
+  // react-app을 사용하기 때문에 React.useState()라고 적지 않고 import할 수 있음
+  const [counter, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  console.log("run all the time.");
+  useEffect(() => {
+    console.log("CALL THE API...run only once...")
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
