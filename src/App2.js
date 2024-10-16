0eb985a764f5ec3bb78583a2e52e55cce35ca037
h-faber <hfaberlu@gmail.com>
2024-10-16 23:27:04 +0900

    useEffect cleanup

---
+++ src/App2.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function Hello() {
+  function byFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("hi :)");
+    // return : cleanup function
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
