6766faf2b6d818c78a91439e5e0d851ce08d83b9
h-faber <hfaberlu@gmail.com>
2024-10-17 21:44:33 +0900

    todo list

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 function App() {
-  // react-app을 사용하기 때문에 React.useState()라고 적지 않고 import할 수 있음
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => {
-    setValue((prev) => prev + 1);
-  };
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("run all the time.");
-  useEffect(() => {
-    // [] : 아무것도 지켜보지 않기 때문에 한 번만 실행됨
-    console.log("run only once.");
-  }, []);
-  useEffect(() => {
-    console.log("run when 'keyword' changes.");
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray])
+        setToDo("");
     }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("run when 'counter & keyword' changes.");
-  }, [counter, keyword]);
-  return (
-    <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-    </div>
-  );
+    return (<div>
+        <h1>My To Dos ({toDos.length})</h1>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
+            <button>Add To Do</button>
+        </form>
+        <hr/>
+        <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+    </div>)
 }
 
 export default App;
